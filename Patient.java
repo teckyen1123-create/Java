c837228eebc975ce8a40c9c06f20ff2d5445636a
TeckYen <teckyen1123@gmail.com>
2026-09-20 04:54:32 +0800

    Create Patient.java

---
+++ Patient.java
@@ -0,0 +1,11 @@
+public class Patient extends User{
+    public Patient(String userID, String name, String username, String password) {
+        super(userID, name, username, password);
+    }
+
+    @Override
+    public void openDashboard() {
+        PatientFrame patient = new PatientFrame();
+        patient.setVisible(true);
+    }
+}
